6a82e754348e4a38e085bc88a7986e79cd4f725a
Sreejith <sreejitherimbranelloor@gmail.com>
2020-01-15 16:48:23 +0530

    - recycler in working state

--- app/src/main/java/com/example/filmsapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/filmsapp/ui/HomeFragment.kt
@@ -1,16 +1,19 @@
 package com.example.filmsapp.ui
-
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.filmsapp.R
+import com.example.filmsapp.adapter.FilmAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
 
 class HomeFragment : Fragment() {
+    val items: ArrayList<String> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,12 +22,32 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val v= inflater.inflate(R.layout.fragment_home, container, false)
         init(v)
+
         return v
     }
 
     private fun init(v: View) {
 
+//        val filmName = v.findViewById<TextView>(R.id.film_name)
+//        val releaseYear=v.findViewById<TextView>(R.id.release_year)
+//        val process = FetchData()
+//        process.execute()
+        additems()
 
+        v.home_recycler.layoutManager = LinearLayoutManager(context)
+
+        v.home_recycler.adapter = FilmAdapter(items, activity as Context)
+
+
+    }
+
+    private fun additems() {
+        items.add("adsadasd")
+        items.add("adsadasd")
+        items.add("adsadasd")
+        items.add("adsadasd")
+        items.add("adsadasd")
+        items.add("adsadasd")
     }
 
 }
