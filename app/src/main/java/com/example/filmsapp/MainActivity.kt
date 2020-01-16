f50a620f6221bd02bd508edee689fe0471507338
Sreejith <sreejitherimbranelloor@gmail.com>
2020-01-16 13:15:09 +0530

    - MainActivity final commit

--- app/src/main/java/com/example/filmsapp/MainActivity.kt
+++ app/src/main/java/com/example/filmsapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.filmsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.filmsapp.ui.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+        if(savedInstanceState==null)
+        {
+            val fragment=HomeFragment()
+            supportFragmentManager.beginTransaction().add(R.id.main_content,fragment).addToBackStack("stack").commit()
+        }
     }
 }
