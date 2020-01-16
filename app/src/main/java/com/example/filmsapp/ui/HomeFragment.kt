a2fc09aa4a6718024573e1320129c2d1f50e9baf
Sreejith <sreejitherimbranelloor@gmail.com>
2020-01-16 13:13:47 +0530

    - Home page working properly(recycler view) commit

--- app/src/main/java/com/example/filmsapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/filmsapp/ui/HomeFragment.kt
@@ -1,53 +1,111 @@
 package com.example.filmsapp.ui
+
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.filmsapp.R
+import com.example.filmsapp.adapter.FetchData
 import com.example.filmsapp.adapter.FilmAdapter
-import kotlinx.android.synthetic.main.fragment_home.*
+import com.example.filmsapp.data.Filmdata
 import kotlinx.android.synthetic.main.fragment_home.view.*
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
 
 
 class HomeFragment : Fragment() {
-    val items: ArrayList<String> = ArrayList()
+    val itemLlist: ArrayList<Filmdata> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val v= inflater.inflate(R.layout.fragment_home, container, false)
+        val v = inflater.inflate(R.layout.fragment_home, container, false)
         init(v)
+        fetchData()
 
         return v
     }
 
     private fun init(v: View) {
 
-//        val filmName = v.findViewById<TextView>(R.id.film_name)
-//        val releaseYear=v.findViewById<TextView>(R.id.release_year)
-//        val process = FetchData()
-//        process.execute()
-        additems()
+        val filmName = v.findViewById<TextView>(R.id.film_name)
+        val process = FetchData()
+        process.execute()
 
         v.home_recycler.layoutManager = LinearLayoutManager(context)
 
-        v.home_recycler.adapter = FilmAdapter(items, activity as Context)
+        v.home_recycler.adapter = FilmAdapter(this.itemLlist, activity as Context)
 
 
     }
 
-    private fun additems() {
-        items.add("adsadasd")
-        items.add("adsadasd")
-        items.add("adsadasd")
-        items.add("adsadasd")
-        items.add("adsadasd")
-        items.add("adsadasd")
+    private fun fetchData() {
+        val mClient = OkHttpClient();
+        val request = Request.Builder()
+            .url("https://swapi.co/api/films/")
+            .addHeader("Content-Type", " application/x-www-form-urlencoded")
+            .build()
+
+        mClient.newCall(request).enqueue(object : Callback {
+
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                activity!!.runOnUiThread {
+                    Toast.makeText(activity, "Failed", Toast.LENGTH_LONG)
+
+                }
+                e.printStackTrace()
+            }
+
+
+            override fun onResponse(call: okhttp3.Call, response: Response) {
+                if (response.isSuccessful) {
+                    val responseString = response.body!!.string()
+                    Log.d("loga", "succ: " + responseString)
+
+                    activity!!.runOnUiThread {
+
+                        try {
+                            val jsonObj = JSONObject(responseString)
+                            val JA = jsonObj.getJSONArray("results")
+                            for (i in 0 until JA.length()) {
+                                val JO = JA[i] as JSONObject
+                                val model = Filmdata(
+                                    JO.getString("title"),
+                                    JO.getString("director"),
+                                    JO.getString("producer"),
+                                    JO.getString("release_date"),
+                                    JO.getString("opening_crawl")
+                                )
+                                itemLlist.add(model)
+                                view!!.home_recycler.adapter = FilmAdapter(itemLlist, activity as Context)
+
+                            }
+
+
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+
+                    }
+                }
+            }
+
+        })
+
     }
+
 
 }
