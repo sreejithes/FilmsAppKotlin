363d24bcdf3ac25e7804c8ae9dbcd2628681af91
Sreejith <sreejitherimbranelloor@gmail.com>
2020-01-16 17:02:15 +0530

    -removed new line character from the description data

--- app/src/main/java/com/example/filmsapp/ui/DetailsFragment.kt
+++ app/src/main/java/com/example/filmsapp/ui/DetailsFragment.kt
@@ -1,33 +1,29 @@
 package com.example.filmsapp.ui
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-
+import com.example.filmsapp.MainActivity
 import com.example.filmsapp.R
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_details.view.*
 
-class DetailsFragment : Fragment() {
-
+class DetailsFragment(var title:String, var directorName: String, var producerName: String, var releaseDate : String, var description:String) : Fragment()
+{
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val v= inflater.inflate(R.layout.fragment_details, container, false)
-        iniit(v)
+        description=description.replace("\n","")
+        v.film_title.text = title
+        v.film_director.text=directorName
+        v.film_producer.text=producerName
+        v.film_release_date.text=releaseDate
+        v.film_description.text=description
         return v
     }
-
-    private fun iniit(v: View) {
-
-        val details=v.findViewById<TextView>(R.id.film_details)
-        val title=v.findViewById<TextView>(R.id.film_title)
-
-    }
-
 }
