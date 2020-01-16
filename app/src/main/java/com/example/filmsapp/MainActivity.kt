7d6d726062b449313288b3a05004ee8d35fee4ea
Sreejith <sreejitherimbranelloor@gmail.com>
2020-01-16 15:03:31 +0530

    -added snackbar for showing network status

--- app/src/main/java/com/example/filmsapp/MainActivity.kt
+++ app/src/main/java/com/example/filmsapp/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import com.example.filmsapp.ui.HomeFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
