3c72f80d38a92cf1bc91ee24898385b1b33e88a7
Sreejith <sreejitherimbranelloor@gmail.com>
2020-01-16 13:10:16 +0530

    - data model for film details final commit

--- app/src/main/java/com/example/filmsapp/data/Filmdata.kt
+++ app/src/main/java/com/example/filmsapp/data/Filmdata.kt
@@ -2,5 +2,5 @@ package com.example.filmsapp.data
 data class Filmdata(var filmName : String,
                     var directorName: String,
                     var producerName: String,
-                    var releaseData : String,
-                    var Description:String)
+                    var releaseDate : String,
+                    var description:String)
