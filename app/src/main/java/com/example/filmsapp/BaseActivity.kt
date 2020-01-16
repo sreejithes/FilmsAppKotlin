9c66f3ddeb9d9e5c2eeeb73e28405b64c768c2f1
Sreejith <sreejitherimbranelloor@gmail.com>
2020-01-16 17:00:55 +0530

    -deleted some unused codes and comments

--- app/src/main/java/com/example/filmsapp/BaseActivity.kt
+++ app/src/main/java/com/example/filmsapp/BaseActivity.kt
@@ -7,14 +7,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
-
-/** Author : http://devdeeds.com
- *  Project : Sample Project - Internet status checking
- *  Date : 24 Feb 2018*/
-
-
-//THIS IS THE BASE ACTIVITY OF ALL ACTIVITIES OF THE APPLICATION.
-
 @SuppressLint("Registered")
 open class BaseActivity : AppCompatActivity(), ConnectivityReceiver.ConnectivityReceiverListener {
     private var mSnackBar: Snackbar? = null
@@ -25,35 +17,24 @@ open class BaseActivity : AppCompatActivity(), ConnectivityReceiver.Connectivity
             IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
     }
 
-
     @SuppressLint("WrongConstant")
     private fun showMessage(isConnected: Boolean) {
-
-
 
         if (!isConnected) {
 
             val messageToUser = "You are offline now."
-
             mSnackBar = Snackbar.make(findViewById(R.id.main_content), messageToUser, Snackbar.LENGTH_LONG) //Assume "rootLayout" as the root layout of every activity.
             mSnackBar?.duration = Snackbar.LENGTH_INDEFINITE
             mSnackBar?.show()
         } else {
             mSnackBar?.dismiss()
         }
-
-
     }
-
     override fun onResume() {
         super.onResume()
 
         ConnectivityReceiver.connectivityReceiverListener = this
     }
-
-    /**
-     * Callback will be called when there is change
-     */
     override fun onNetworkConnectionChanged(isConnected: Boolean) {
         showMessage(isConnected)
     }
